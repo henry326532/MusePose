f9b84ca3528939f4e714662620623ace9aaed80c
henry326532 <38341862+henry326532@users.noreply.github.com>
2024-06-04 11:26:22 +0800

    Update downloading_weights.py

--- downloading_weights.py
+++ downloading_weights.py
@@ -2,12 +2,14 @@ import os
 import requests
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
+import concurrent
 
 def download_file(session, url, path):
     local_filename = url.split('/')[-1]
+    full_path = os.path.join(path, local_filename)
     with session.get(url, stream=True) as r:
         r.raise_for_status()
-        with open(os.path.join(path, local_filename), 'wb') as f:
+        with open(full_path, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
     return local_filename
@@ -15,7 +17,8 @@ def download_file(session, url, path):
 def download_files(urls, paths):
     with requests.Session() as session:
         with ThreadPoolExecutor(max_workers=5) as executor:
-            futures = [executor.submit(download_file, session, url, path) for url, path in zip(urls, paths)]
+            futures = [executor.submit(download_file, session, url, os.path.join(base_dir, path))
+                       for url, path in zip(urls, paths)]
             for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures), desc="Downloading files"):
                 try:
                     future.result()
@@ -23,7 +26,6 @@ def download_files(urls, paths):
                     print(f"Download failed: {e}")
 
 base_dir = 'pretrained_weights'
-
 urls = ['https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_l_8x8_300e_coco/yolox_l_8x8_300e_coco_20211126_140236-d3bd2b23.pth',
         'https://huggingface.co/yzd-v/DWPose/resolve/main/dw-ll_ucoco_384.pth',
         'https://huggingface.co/TMElyralab/MusePose/resolve/main/MusePose/denoising_unet.pth',
@@ -36,9 +38,10 @@ urls = ['https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_l_8x8_300e_
         ]
 
 paths = ['dwpose', 'dwpose', 'MusePose', 'MusePose', 'MusePose', 'MusePose', 'sd-image-variations-diffusers/unet', 'image_encoder', 'sd-vae-ft-mse']
+
 # Create directories
 for path in set(paths):
-    os.makedirs(path, exist_ok=True)
+    os.makedirs(os.path.join(base_dir, path), exist_ok=True)
 
 # Download files
 download_files(urls, paths)
